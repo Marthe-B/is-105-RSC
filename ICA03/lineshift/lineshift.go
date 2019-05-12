d3bc64a006211f3cab13db002950f483c9e795d9
Ole Christian Johannessen <ole.christian.j@hotmail.com>
2019-05-12 20:32:27 +0200

    ica3 oppg 1 og 5

--- ICA03/lineshift/lineshift.go
+++ ICA03/lineshift/lineshift.go
@@ -3,23 +3,33 @@ package main
 import (
 	"fmt"
 	"os"
+
 	"../fileutils"
 )
 
-func lineshift(text []byte) {	
-	carriageReturn := 0
-	linefeed := 0
+func lineshift(text []byte) {
+	carriageReturn := 0 // \r
+	linefeed := 0       // \n
+	crlf := 0           // \r\n
 	for i := 0; i < len(text); i++ {
-		if (text[i] == ("\x0A")[0]) {
+		if text[i] == ("\x0A")[0] { // = \n
 			linefeed++
-		} else if (text[i] == ("\x0D")[0]) {
-			carriageReturn++
+		} else if text[i] == ("\x0D")[0] {
+			if len(text) > i && text[i+1] == ("\x0A")[0] { // = \r\n
+				crlf++
+				i++
+			} else { // = \r
+				carriageReturn++
+			}
+
 		}
-			
+
 	}
 
-	fmt.Println("Carriage returns: ", carriageReturn)
-	fmt.Println("Linefeeds: ", linefeed)
+	fmt.Println("Antall brukte newline tegn funnet i filen:")
+	fmt.Println("CR (Commodore, Apple II family, classic Mac OS): ", carriageReturn)
+	fmt.Println("LF (Unix, Linux, macOS, FreeBSD ...): ", linefeed)
+	fmt.Println("CR LF (MS Windows, DOS, Symbian OS ...): ", crlf)
 }
 
 func main() {
@@ -28,7 +38,7 @@ func main() {
 
 	text := fileutils.FileToByteslice(arg1)
 
-	fmt.Printf("%q \n\n" , text)
+	fmt.Printf("%q \n\n", text)
 
 	lineshift(text)
 }
